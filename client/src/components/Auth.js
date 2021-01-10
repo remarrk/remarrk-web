e58e8c2cdee11254ee94ad300487f9042911ba03
Mathew de Vin <mathewdevin03@gmail.com>
2021-01-10 03:32:57 -0800

    Login/Signup UI change, error messages, sign in redirection

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -3,37 +3,48 @@ import "firebase/auth";
 
 export const signInGoogleAuth = (e) => {
   e.preventDefault();
-  const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth().signInWithPopup(googleAuthProvider)
-    .then((user) => {
-      console.log("Logged in user successfully", user);
-    })
-    .catch((error) => {
-      console.log(error.code, error.message);
-    });
+  return new Promise(((resolve, reject) => {
+    const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(googleAuthProvider)
+      .then((user) => {
+        console.log("Logged in user successfully", user);
+        resolve(user);
+      })
+      .catch((error) => {
+        console.log(error.code, error.message);
+        reject(error);
+      });
+  }));
 }
 
 export const signInEmail = (e, email, password) => {
   e.preventDefault();
-  firebase.auth().signInWithEmailAndPassword(email, password)
-    .then((user) => {
-      console.log("Logged in user successfully", user);
-    })
-    .catch((error) => {
-      console.log(error.code, error.message);
-    });
+  return new Promise(((resolve, reject) => {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then((user) => {
+        console.log("Logged in user successfully", user);
+        resolve(user);
+      })
+      .catch((error) => {
+        console.log(error.code, error.message);
+        reject(error);
+      });
+  }));
 }
 
 export const signUpEmail = (e, email, password) => {
   e.preventDefault();
-  firebase.auth().createUserWithEmailAndPassword(email, password)
-    .then((user) => {
-      console.log("Created user successfully");
-      // todo: log the user in
-    })
-    .catch((error) => {
-      console.log(error.code, error.message);
-    });
+  return new Promise(((resolve, reject) => {
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then((user) => {
+        console.log("Created user successfully");
+        resolve(user);
+      })
+      .catch((error) => {
+        console.log(error.code, error.message);
+        reject(error);
+      });
+  }));
 }
 
 export const signOut = (e) => {
