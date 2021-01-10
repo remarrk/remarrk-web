7daf1bf8adf19236d8cbeb25a40faa66be54eea6
opeyem1a <opeyadeyemi@gmail.com>
2021-01-09 16:48:58 -0800

    adding remark component

---
+++ client/src/components/Remark.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function Remark() {
+    return (
+        <div>Hi</div>
+    );
+};
+
+export default Remark;
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Remark from './components/Remark';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Remark />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
