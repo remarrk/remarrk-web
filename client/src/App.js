942314f570ec94bf721e277b50b4a6a70024616b
Mathew de Vin <mathewdevin03@gmail.com>
2021-01-09 22:41:21 -0800

    Login and signup email functionality

--- client/src/App.js
+++ client/src/App.js
@@ -5,39 +5,35 @@ import './styles/styles.scss';
 
 import firebase from "firebase/app";
 import "firebase/auth";
-import {FirebaseAuthProvider, FirebaseAuthConsumer, IfFirebaseAuthed, IfFirebaseAuthedAnd} from '@react-firebase/auth';
-import {firebaseConfig} from "./components/config/firebaseConfig.js";
+import {FirebaseAuthProvider, FirebaseAuthConsumer} from '@react-firebase/auth';
+import {firebaseConfig} from "./components/config/FirebaseConfig.js";
+
+import {signOut} from './components/Auth.js';
+
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 function App() {
   return (
     <FirebaseAuthProvider {...firebaseConfig} firebase={firebase}>
       <div className='App'>
-        <button onClick={() => {
-          const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
-          firebase.auth().signInWithPopup(googleAuthProvider);
-        }}>
-          Sign In With Google
-        </button>
-        <button
-          onClick={() => {
-            firebase.auth().signOut();
-          }}>
-          Sign Out
-        </button>
+        <Login/>
+        <Signup/>
+        <button onClick={signOut}>Sign Out</button>
         <FirebaseAuthConsumer>
           {({isSignedIn}) => {
             if (isSignedIn) {
               return (
-                <div>
+                <>
                   <Remark editable={true}/>
                   <Remark editable={false}/>
-                </div>
+                </>
               );
             } else {
               return (
-                <div>
+                <>
                   <h1>You are not logged in</h1>
-                </div>
+                </>
               )
             }
           }}
