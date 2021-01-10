02c19ca17d1e0e28fd6fd17c71091f8f333c5f8b
Mathew de Vin <mathewdevin03@gmail.com>
2021-01-09 23:59:53 -0800

    State management integration and navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Remark from './components/Remark';
 import './styles/styles.scss';
@@ -8,31 +8,62 @@ import "firebase/auth";
 import {FirebaseAuthProvider, FirebaseAuthConsumer} from '@react-firebase/auth';
 import {firebaseConfig} from "./components/config/FirebaseConfig.js";
 
-import {signOut} from './components/Auth.js';
-
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import Navbar from "./components/Navbar";
 
 function App() {
+  /* STATES ID's
+   * NOT LOGGED IN:
+   * 0 -> Home
+   * 1 -> Log In
+   * 2 -> Sign Up
+   * LOGGED IN:
+   * 0 -> Home
+   * 1 -> Favourites
+   */
+  const [state, setState] = useState(0);
+
+  function setAppState(e, id) {
+    e.preventDefault();
+    setState(id);
+  }
+
   return (
     <FirebaseAuthProvider {...firebaseConfig} firebase={firebase}>
       <div className='App'>
-        <Login/>
-        <Signup/>
-        <button onClick={signOut}>Sign Out</button>
         <FirebaseAuthConsumer>
           {({isSignedIn}) => {
             if (isSignedIn) {
               return (
                 <>
-                  <Remark editable={true}/>
-                  <Remark editable={false}/>
+                  <Navbar signedIn={isSignedIn} onClick={setAppState}/>
+                  {
+                    state === 0 ?
+                      <>
+                        <Remark editable={true}/>
+                        <Remark editable={false}/>
+                      </>
+                      :
+                      <>
+                        <h1>Favourites</h1>
+                      </>
+                  }
                 </>
               );
             } else {
               return (
                 <>
-                  <h1>You are not logged in</h1>
+                  <Navbar signedIn={isSignedIn} onClick={setAppState}/>
+                  {
+                    state === 0 ?
+                      <h1>You are not logged in</h1>
+                      :
+                      state === 1 ?
+                        <Login/>
+                        :
+                        <Signup/>
+                  }
                 </>
               )
             }
