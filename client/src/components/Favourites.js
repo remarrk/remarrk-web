a8750130353537feeb5e4d7efd01b50dc7136e65
Jaden Balogh <jadenbalogh@gmail.com>
2021-01-10 12:38:51 -0800

    Revert "Update Favourites.js"
    This reverts commit 717e9955a1c957a9689946e41fa611c04f590db7.

--- client/src/components/Favourites.js
+++ client/src/components/Favourites.js
@@ -2,23 +2,21 @@ import React from 'react';
 import axios from "axios";
 
 function Favourites(props) {
-
   const getFavourites = (userId) => {
     axios.get(`http://localhost:3001/get-favourites?userId=${userId}`)
       .then((res) => {
-        return res.data;
+        console.log(res);
+        let favourite = res.data;
       })
       .catch((err) => {
         console.log(err)
-        return null;
       })
   }
 
   return (
     <div>
       {
-        getFavourites(props.userId).map(favourite => console.log(favourite.message))
-        //favourites.map(favourite => <div>{favourite.message}</div>)
+        getFavourites(props.userId)
       }
     </div>
   );
