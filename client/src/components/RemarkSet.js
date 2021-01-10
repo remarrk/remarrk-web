f1ccb750a76fb375e97d777afd17dd2a6635b44d
opeyem1a <opeyadeyemi@gmail.com>
2021-01-10 11:47:39 -0800

    just formatting

--- client/src/components/RemarkSet.js
+++ client/src/components/RemarkSet.js
@@ -1,24 +1,28 @@
-import { useState } from 'react';
-import Remark from './Remark';
-import LongButton from './LongButton';
-import '../styles/remarkSet.scss';
+import { useState } from "react";
+import Remark from "./Remark";
+import LongButton from "./LongButton";
+import "../styles/remarkSet.scss";
 
 function RemarkSet() {
-    let [editable, setEditable] = useState(false);
-    let [buttonText, setButtonText] = useState('+ New');
+  let [editable, setEditable] = useState(false);
+  let [buttonText, setButtonText] = useState("+ New");
 
-    const toggleEditable = () => {
-        console.log(`toggled editable ${editable}`);
-        setEditable(!editable);
-        setButtonText(editable ? `+ New` : `Browse`);
-    };
+  const toggleEditable = () => {
+    console.log(`toggled editable ${editable}`);
+    setEditable(!editable);
+    setButtonText(editable ? `+ New` : `Browse`);
+  };
 
   return (
     <div className={`remark-flex-wrap`}>
-    <div className={`remark-flex-side`}>
-      <Remark className={`remark-flex-item`} editable={editable} />
-      <LongButton buttonText={buttonText} editable={editable} onModeChange={toggleEditable} />
-    </div>
+      <div className={`remark-flex-side`}>
+        <Remark className={`remark-flex-item`} editable={editable} />
+        <LongButton
+          buttonText={buttonText}
+          editable={editable}
+          onModeChange={toggleEditable}
+        />
+      </div>
     </div>
   );
 }
