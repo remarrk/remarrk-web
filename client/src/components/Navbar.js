77d88e3c4069158a9b7dd376d465617209fead10
opeyem1a <opeyadeyemi@gmail.com>
2021-01-16 15:31:23 -0800

    purging code for rebuild

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,34 +1,14 @@
-import React from 'react';
-import {signOut} from './Auth.js';
+import React from "react";
+import { signOut } from "./Auth.js";
 
-import '../styles/navbar.scss';
+import "../styles/navbar.scss";
 
-function Navbar(props) {
-  function signOutAccount(e) {
-    signOut(e);
-    props.onClick(e, 0);
-  }
-
+function Navbar() {
   return (
     <div className="nav-bar">
-      <h2 className="nav-title" onClick={e => props.onClick(e, 0)}>remarrk.</h2>
-      {
-        props.signedIn ?
-          <div className="nav-navigation">
-            <button onClick={e => props.onClick(e, 1)}>favourites</button>
-            <button onClick={signOutAccount}>sign out</button>
-          </div>
-          :
-          <div className="nav-navigation">
-            {
-              props.state === 2 ?
-                null
-                :
-                <button onClick={e => props.onClick(e, 2)}>login</button>
-            }
-
-          </div>
-      }
+      <h2 className="nav-title">
+        remarrk.
+      </h2>
     </div>
   );
 }
